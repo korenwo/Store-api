49ce1e80f92c93cd7d2b042e88e885db4dbc752f
koren wohl <korenwohl@gmail.com>
2021-08-28 16:00:40 +0300

    save

--- src/config/enviorment/production.js
+++ src/config/enviorment/production.js
@@ -3,3 +3,5 @@ module.exports = {
     port: process.env.PORT,
     dbUrl: process.env.DB_URL
 };
+
+mongodb+srv://korenford:fordfiesta@fordfiesta@cluster0.bzzjj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
