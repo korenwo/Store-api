a73009a8c67be0cd2aa48ec83ee57dde64c45317
koren wohl <korenwohl@gmail.com>
2021-09-18 14:25:22 +0300

    save

--- src/config/environment/production.js
+++ src/config/environment/production.js
@@ -1,5 +1,5 @@
 module.exports = {
+    jwtSecret: "Koren123",
     port: process.env.PORT,
     dbUrl: process.env.DB_URL
 };
-
--- src/server.js
+++ src/server.js
@@ -1,31 +1,25 @@
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./config/routes');
-const app = express();
 const { port, dbUrl } = require('./config/environment/index');
 
-app.options('*', cors()) // include before other routes 
-app.use(cors())
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(routes);
 
-app.get('/', (req, res) => {
-    res.send('works!');
-});
-
 mongoose.connect(dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 });
-
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-    app.listen(port, () => {
-        console.log('Listening on http://localhost:' + port);
-    });
+	app.listen(port, () => {
+		console.log('Listening on http://localhost:' + port);
+	});
 });
